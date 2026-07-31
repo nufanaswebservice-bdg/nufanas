0462f71684e09b98ba039d0b1a43a6e7c64b9d6a
Nufanas <hello@nufanas.com>
2026-07-31 17:54:00 +0700

    feat: add Google Analytics / Ads tag (G-TD4NKD79DS)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Inter, Plus_Jakarta_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { SITE_CONFIG } from "@/lib/constants";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -102,6 +103,19 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${jakarta.variable} font-sans antialiased`}
       >
+        {/* Google Tag (gtag.js) - Google Ads */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-TD4NKD79DS"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TD4NKD79DS');
+          `}
+        </Script>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
